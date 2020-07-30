5537d0bef7683a4bcbe153a031167129cca89d45
vinothsmart <s.vinothkanna90@gmail.com>
2020-07-30 16:52:22 +0530

    AuthContrl added

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -3,9 +3,83 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->user = new User;
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+            [
+                "firstname" => "required|string",
+                "lastname" => "required|string",
+                "email" => "required|string",
+                "password" => "required|string|min:6",
+            ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->messages()->toArray(),
+            ], 400);
+        }
+
+        $checkEmail = $this->user->where("email", $request->email)->count();
+        if ($checkEmail > 0) {
+            return response()->json([
+                "success" => false,
+                "message" => "this email already exists please try another email",
+            ], 422);
+        }
+
+        $registerComplete = $this->user::create([
+            "firstname" => $request->firstname,
+            "lastname" => $request->lastname,
+            "email" => $request->email,
+            "password" => $request->password,
+        ]);
+
+        if ($registerComplete) {
+            $this->login($request);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->only("email", "password"),
+            [
+                "email" => "required|string",
+                "password" => "required|string|min:6",
+            ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->messages()->toArray(),
+            ], 400);
+        }
+
+        $jwtToken = null;
+
+        $input = $request->only("email", "password");
+
+        if (!$jwtToken = auth("users")->attempt($input)) {
+            return response()->json([
+                "success" => false,
+                "message" => "invalid email or password",
+            ], 400);
+        }
+
+        return response()->json([
+            "success" => true,
+            "token" => $jwtToken,
+        ], 400);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname', 'email', 'password', 'lastname',
     ];
 
     /**
