1e2f733e2a803e8b972d20c5c56ffa35a7d55782
vinothsmart <s.vinothkanna90@gmail.com>
2020-10-07 10:16:56 +0530

    routes added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +9,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
 });
